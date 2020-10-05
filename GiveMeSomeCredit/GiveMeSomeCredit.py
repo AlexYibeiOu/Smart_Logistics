76526b098e481ed3340f164599022a6e8d7df050
yibeiou <yibei.ou@gmail.com>
2020-10-05 22:09:11 +1100

    Update GiveMeSomeCredit.py

--- GiveMeSomeCredit/GiveMeSomeCredit.py
+++ GiveMeSomeCredit/GiveMeSomeCredit.py
@@ -1,17 +1,22 @@
 import pandas as pd 
 
-# Load data
+# Load - data
 df_train = pd.read_csv('./data/cs-training.csv')
 df_train
 
-# drop the first column
+# drop - the first column
 df_train = df_train.iloc[:, 1:]
 df_train
 
+# explore - SeriousDlqin2yrs
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.countplot(x='SeriousDlqin2yrs', data=df_train)
 print("percentage {}".format(df_train['SeriousDlqin2yrs'].sum()/len(df_train)))
 
+# Check - missing data
 null_num = df_train.isnull().sum()
 pd.DataFrame({'Column': null_num.index, 'Value':null_num.values, '%':null_num.values/len(df_train)})
+
+df_train['RevolvingUtilizationOfUnsecuredLines'].describe()
+sns.distplot(df_train['RevolvingUtilizationOfUnsecuredLines'])
